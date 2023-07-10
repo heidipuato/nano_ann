f0e31d2876b88d51c0e2bb37ac0f4597ff44a1f4
heidipuato <heidipuato@gmail.com>
2023-07-10 21:01:06 +0800

    for deploy vercel

--- test_app/nanocheck/urls.py
+++ test_app/nanocheck/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('/',views.home, name='home'),
     path('test/',views.test, name='test'),
     path('predict/',views.predict, name='predict'),
     path('about/',views.about, name='about'),
